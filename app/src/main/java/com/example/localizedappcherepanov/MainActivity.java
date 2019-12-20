f8fbb77300c5ae3237348844c2af6cabe8b0d3f0
jembiq <vadim.vadim4@yandex.ru>
2019-12-20 19:37:03 +0300

    new proj

--- app/src/main/java/com/example/localizedappcherepanov/MainActivity.java
+++ app/src/main/java/com/example/localizedappcherepanov/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.localizedappcherepanov;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.LocaleList;
@@ -17,7 +18,9 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
 
     private static Spinner mLocaleSpinner;
-    private static Button mSwitchBtn;
+    private static Spinner mThemeSwitchSpinner;
+    private static Button mSwitchLangBtn;
+    private static Button mSwitchColorBtn;
     private static Locale localeRU = new Locale("ru");
     private static Locale localeEN = new Locale("en");
     private static Locale localeJP = new Locale("ja");
@@ -26,14 +29,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Utils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_main);
         init();
     }
 
     private void init() {
-        mSwitchBtn = findViewById(R.id.switchLangBtn);
+        mSwitchLangBtn = findViewById(R.id.switchLangBtn);
+        mSwitchColorBtn = findViewById(R.id.switchColorBtn);
         mLocaleSpinner = findViewById(R.id.localeSpinner);
+        mThemeSwitchSpinner = findViewById(R.id.themeSwitchSpinner);
         initLocaleSpinner();
+        initThemeSwitchSpinner();
     }
 
     private void initLocaleSpinner() {
@@ -57,32 +64,84 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void initThemeSwitchSpinner() {
+        ArrayAdapter<CharSequence> themeAdapter = ArrayAdapter.createFromResource(this
+                , R.array.colors
+                , android.R.layout.simple_spinner_item);
+        themeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mThemeSwitchSpinner.setAdapter(themeAdapter);
+
+        mThemeSwitchSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                changeColor(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
     private void changeLanguage(String language) {
         switch (language) {
             case "Русский" :
                 config.setLocale(localeRU);
-                switchBtnAction();
+                switchLanguageBtnAction();
                 break;
 
             case "English" :
                 config.setLocale(localeEN);
-                switchBtnAction();
+                switchLanguageBtnAction();
                 break;
 
             case "日本語" :
                 config.setLocale(localeJP);
-                switchBtnAction();
+                switchLanguageBtnAction();
 
             default:break;
         }
     }
 
-    private void switchBtnAction() {
-        mSwitchBtn.setOnClickListener(new View.OnClickListener() {
+    private void changeColor(int position) {
+        switch (position) {
+            case 0 :
+                Utils.changeToTheme(Utils.THEME_DEFAULT);
+                switchThemeBtnAction();
+                break;
+            case 1 :
+                Utils.changeToTheme(Utils.THEME_GREEN);
+                switchThemeBtnAction();
+                break;
+            case 2 :
+                Utils.changeToTheme(Utils.THEME_BLACK);
+                switchThemeBtnAction();
+                break;
+            case 3 :
+                Utils.changeToTheme(Utils.THEME_BLUE);
+                switchThemeBtnAction();
+                break;
+            default:
+        }
+    }
+
+    private void switchLanguageBtnAction() {
+        mSwitchLangBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
                 recreate();
+            }
+        });
+    }
+
+    private void switchThemeBtnAction() {
+        mSwitchColorBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
             }
         });
     }
---
+++ app/src/main/java/com/example/localizedappcherepanov/Utils.java
@@ -0,0 +1,35 @@
+package com.example.localizedappcherepanov;
+
+import android.app.Activity;
+import android.content.Intent;
+
+public class Utils {
+    private static int sTheme;
+
+    public final static int THEME_DEFAULT = 0;
+    public final static int THEME_GREEN= 1;
+    public final static int THEME_BLACK = 2;
+    public final static int THEME_BLUE = 3;
+
+    public static void changeToTheme(int theme) {
+        sTheme = theme;
+    }
+
+    public static void onActivityCreateSetTheme(Activity activity) {
+        switch (sTheme) {
+            case THEME_DEFAULT :
+                activity.setTheme(R.style.AppTheme);
+                break;
+            case THEME_GREEN :
+                activity.setTheme(R.style.ThemeGreen);
+                break;
+            case THEME_BLACK :
+                activity.setTheme((R.style.ThemeBlack));
+                break;
+            case THEME_BLUE :
+                activity.setTheme(R.style.ThemeBlue);
+                break;
+            default:
+        }
+    }
+}
